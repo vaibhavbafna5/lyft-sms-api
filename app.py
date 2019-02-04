ac1ecfbdb403eb58aa2839f3701f088a0ecd550b
Vaibhav Bafna <vbafna@umich.edu>
2019-02-04 10:27:44 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, abort, make_response
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/", methods=['GET'])
+def say_hi():
+    return "Hello."
